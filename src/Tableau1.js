f36c26b5bbb2fbc708f9d7b934b674678ff21747
ilonaaaaa <92451528+ilonaaaaa@users.noreply.github.com>
2022-01-10 15:55:37 +0100

    uwu

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -10,9 +10,12 @@ class Tableau1 extends Phaser.Scene{
         this.hauteur = 500;
         this.largeur = 1000;
 
-        this.balle=this.physics.add.sprite(this.largeur/2,this.hauteur/2,'balle');
+        this.balle=this.physics.add.sprite(this.largeur/2,this.hauteur/2,'balle').setOrigin(0,0);
         this.balle.setDisplaySize(20,20);
-        this.balle.body.setBounce(1,1);
+        this.balle.body.setBounce(1.1,1.1);
+        this.balle.setVelocityX(Phaser.Math.Between(-200,200))
+        this.balle.setVelocityY(Phaser.Math.Between(-200,200))
+        this.balle.body.setMaxVelocity(1000,1000)
 
         this.haut=this.physics.add.sprite(0,0,'carre').setOrigin(0,0);
         this.haut.setDisplaySize(this.largeur,20);
@@ -26,10 +29,28 @@ class Tableau1 extends Phaser.Scene{
 
         this.physics.add.collider(this.balle,this.bas);
         this.physics.add.collider(this.balle,this.haut);
+        this.initKeyboard();
 
 
     }
+
+    //faire une raquette à gauche et à droite (ZS et OL) pour pouvoir jouer à deux
+    //raquette de 100 pixels de haut et 20 de large
+
+    initKeyboard(){
+
+    }
+
     update(){
+        if(this.balle.x > this.largeur){
+            this.balle.x = 0
+        }
+        if(this.balle.y < 0){
+            this.balle.y = 0
+        }
+        if(this.balle.y > this.hauteur){
+            this.balle.y = this.hauteur
+        }
 
     }
 }
